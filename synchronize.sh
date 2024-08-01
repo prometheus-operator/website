147cbfcca68ed37db6411a29499e80f830d7f10f
Ashwin Sriram <116557116+AshwinSriram11@users.noreply.github.com>
2024-08-01 23:31:23 +0530

    Nit-spellchecks

--- synchronize.sh
+++ synchronize.sh
@@ -10,7 +10,7 @@ if [[ -z "$USE_LOCAL_REPOSITORIES" ]]; then
   git clone https://github.com/AshwinSriram11/prometheus-operator -b design.md --depth 1 repos/prometheus-operator
   git clone https://github.com/prometheus-operator/kube-prometheus -b main --depth 1 repos/kube-prometheus
 else
-  ln -s ../../prometheus-operatorus-operator repos/prometheus-operator 
+  ln -s ../../prometheus-operator repos/prometheus-operator 
   ln -s ../../kube-prometheus repos/kube-prometheus
 fi
 
