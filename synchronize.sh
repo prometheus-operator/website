77738ce8ec3dd6bb45312fde443c58854fa2d337
Ashwin <ashwinsriram11@gmail.com>
2024-08-01 23:28:11 +0530

    Rewrite design

--- synchronize.sh
+++ synchronize.sh
@@ -6,10 +6,11 @@ rm -rf repos/
 mkdir repos/
 
 if [[ -z "$USE_LOCAL_REPOSITORIES" ]]; then
-  git clone https://github.com/prometheus-operator/prometheus-operator -b main --depth 1 repos/prometheus-operator
+  # git clone https://github.com/prometheus-operator/prometheus-operator -b main --depth 1 repos/prometheus-operator
+  git clone https://github.com/AshwinSriram11/prometheus-operator -b design.md --depth 1 repos/prometheus-operator
   git clone https://github.com/prometheus-operator/kube-prometheus -b main --depth 1 repos/kube-prometheus
 else
-  ln -s ../../prometheus-operator repos/prometheus-operator 
+  ln -s ../../prometheprometheus-operatorus-operator repos/prometheus-operator 
   ln -s ../../kube-prometheus repos/kube-prometheus
 fi
 
